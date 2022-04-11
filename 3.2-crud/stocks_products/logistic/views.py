6cf65087ea04835c2d0917496385873bf2310247
Daniil Melnikov <dnl.melnikov@yandex.ru>
2022-04-11 12:01:58 +0700

    FIX TASK 3.2

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -2,12 +2,15 @@ from rest_framework.viewsets import ModelViewSet
 
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
+from rest_framework.filters import SearchFilter
+
 
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filterset_fields = ['title', 'description']
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'description']
     # при необходимости добавьте параметры фильтрации
 
 
